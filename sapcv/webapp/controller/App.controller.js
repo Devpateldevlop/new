dfe37f185a23838a0d0bae106be82e984f17b5ae
Dev Patel <devpatel@dinksolutions.com>
2024-12-03 11:53:39 +0000

    first

---
+++ sapcv/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (Controller) => {
+  "use strict";
+
+  return Controller.extend("sapcv.controller.View1", {
+      onInit() {
+      }
+  });
+});
---
+++ sapcv/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sapcv/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
